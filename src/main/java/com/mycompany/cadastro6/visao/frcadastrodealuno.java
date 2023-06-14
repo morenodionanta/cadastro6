66ad2aae5a738da606e44fdef48a26dae3bc5bf2
alexdionanta <128551228+morenodionanta@users.noreply.github.com>
2023-06-14 19:37:43 -0300

    habilita caixa de texto

--- src/main/java/com/mycompany/cadastro6/visao/frcadastrodealuno.java
+++ src/main/java/com/mycompany/cadastro6/visao/frcadastrodealuno.java
@@ -15,6 +15,11 @@ public class frcadastrodealuno extends javax.swing.JFrame {
 	 */
 	public frcadastrodealuno() {
 		initComponents();
+		 edtnome.setEnabled(true);
+                 edtsexo.setEnabled(true);
+                 edtidade.setEnabled(true);
+                 edtmatricula.setEnabled(true);
+		 edtamodeingresso.setEnabled(true);
 	}
 
 	/**
@@ -44,8 +49,7 @@ public class frcadastrodealuno extends javax.swing.JFrame {
                 btedita = new javax.swing.JButton();
                 btexcluir = new javax.swing.JButton();
                 jPanel2 = new javax.swing.JPanel();
-                jScrollPane1 = new javax.swing.JScrollPane();
-                jTextPane1 = new javax.swing.JTextPane();
+                txtresultado = new java.awt.TextArea();
 
                 setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -169,23 +173,21 @@ public class frcadastrodealuno extends javax.swing.JFrame {
                                 .addGap(14, 14, 14))
                 );
 
-                jScrollPane1.setViewportView(jTextPane1);
-
                 javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
                 jPanel2.setLayout(jPanel2Layout);
                 jPanel2Layout.setHorizontalGroup(
                         jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(jPanel2Layout.createSequentialGroup()
-                                .addGap(136, 136, 136)
-                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 439, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(232, 232, 232)
+                                .addComponent(txtresultado, javax.swing.GroupLayout.PREFERRED_SIZE, 325, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 );
                 jPanel2Layout.setVerticalGroup(
                         jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                                .addContainerGap(36, Short.MAX_VALUE)
-                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(21, 21, 21))
+                        .addGroup(jPanel2Layout.createSequentialGroup()
+                                .addGap(32, 32, 32)
+                                .addComponent(txtresultado, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addContainerGap(43, Short.MAX_VALUE))
                 );
 
                 javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -270,12 +272,11 @@ public class frcadastrodealuno extends javax.swing.JFrame {
         private javax.swing.JLabel jLabel1;
         private javax.swing.JPanel jPanel1;
         private javax.swing.JPanel jPanel2;
-        private javax.swing.JScrollPane jScrollPane1;
-        private javax.swing.JTextPane jTextPane1;
         private javax.swing.JLabel lbanodeingresso;
         private javax.swing.JLabel lbidade;
         private javax.swing.JLabel lbmatricula;
         private javax.swing.JLabel lbnome;
         private javax.swing.JLabel lbsexo;
+        private java.awt.TextArea txtresultado;
         // End of variables declaration//GEN-END:variables
 }
